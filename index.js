3d987b17ebe8f34f141ef3078b61b4ad38f798a2
Maslakov Alexander <jmas.ukraine@gmail.com>
2017-11-10 14:17:14 +0200

    Simplify values construction

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ export const withForm = (options={}) => {
         onSubmit,
     } = options;
     return component => props => directive(part => {
-        const values = {...(mapPropsToValues ? mapPropsToValues(props): props)};
+        const values = mapPropsToValues ? mapPropsToValues(props): props;
         const update = values => component({
             ...props,
             values,
